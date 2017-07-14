b342e6030d413b2a65a7d77abd2aab1867dfbd28
youhui1214 <405852890@qq.com>
2017-07-14 11:51:01 +0800

    header
    头部组件开发

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+
 import Hello from '@/components/Hello'
+//  import Helo from '../components/Hello.vue'
+import goods from '../components/goods/goods'
+import ratings from '../components/ratings/ratings'
+import seller from '../components/seller/seller'
+
 
 Vue.use(Router)
 
 export default new Router({
+  linkActiveClass: 'active',
+
   routes: [
-    {
-      path: '/',
-      name: 'Hello',
-      component: Hello
-    }
+    { path: '/', redirect: '/goods', component: goods },
+    { path: '/goods', name: 'goods', component: goods },
+    { path: '/ratings', name: 'ratings', component: ratings },
+    { path: '/seller', name: 'seller', component: seller },
   ]
 })
